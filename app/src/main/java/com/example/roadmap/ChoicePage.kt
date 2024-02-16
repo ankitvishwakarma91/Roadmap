ff659b7063036a10ea77cbb4ac7db2425601c1d2
Ankit Vishwakarma <ankitvishwakarma9137@gmail.com>
2024-02-16 16:33:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/roadmap/ChoicePage.kt
@@ -0,0 +1,11 @@
+package com.example.roadmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChoicePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choice_page)
+    }
+}
---
+++ app/src/main/java/com/example/roadmap/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.roadmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val SignUpPage = findViewById<Button>(R.id.Signup)
+    }
+}
